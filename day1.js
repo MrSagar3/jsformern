7588c2d7df1125a8f26f8a799ee9cdf9f023da15
Sagar <chaudharysagar567@gmail.com>
2024-07-12 14:09:38 +0545

    first commit

---
+++ day1.js
@@ -0,0 +1,34 @@
+//variable =used to store the value
+// var cup="tea"
+// let cup1="tea"
+
+// const cup12="coffee"
+
+// const age=1
+// if(true){
+//     var name="sagar"
+//     console.log(name)
+// }
+// console.log(name)
+
+// const age=1
+// const isreally=true
+// const don="don1"
+
+// let name="sagar"
+// console.log(name+" chaudhary")
+
+// array syntax= [item1,item2,...]
+
+// let friends =["sagar","prabisha","nischal",1,2,true]
+// // console.log(friends)
+// console.log(friends[1]) //->access the particular value of the array
+
+//object : syntax= {item1:value1, item2: value2,...}
+
+let person={
+    name:"sagar",
+    age: 22
+}
+console.log(person.age) //one way to access the value of person . this is used mostly
+// console.log(person['age'] ) //another way to access the value of object. 
